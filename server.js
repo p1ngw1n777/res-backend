3021f83ab036abba74949f452065273671c7d759
pingwin <pingwin@MacBook-Air-Medvedev.local>
2024-04-28 23:11:25 +0700

    fix: hashing passwords during authentication and registration

--- server.js
+++ server.js
@@ -2,9 +2,9 @@ const express = require('express');
 const pgp = require('pg-promise')();
 const cors = require('cors');
 const db = require('./db');
+const bcrypt = require('bcrypt');
 
 //const bodyParser = require('body-parser');
-const bcrypt = require('bcrypt');
 //const jwt = require('jsonwebtoken');
 
 const app = express();
@@ -13,7 +13,6 @@ const port = 3000;
 app.use(cors());
 app.use(express.json());
 
-// Запуск сервера
 app.listen(port, () => {
   console.log(`Сервер запущен на порту ${port}`);
 });
@@ -21,15 +20,17 @@ app.listen(port, () => {
 // Аутентификация пользователя
 app.post('/loginForm', async (req, res) => {
   const { username, password } = req.body.data;
-
-    
-    console.log('введённый: ' + username, password);
     try {
       // Получаем данные пользователя из базы данных
-      const login_user = await db.oneOrNone('SELECT * FROM users WHERE login = $1 AND password = $2', [username, password]);
-      console.log('из базы: ' + login_user.login, login_user.password)
-                  
-      if (login_user.login === username && login_user.password === password)
+      const login_user = await db.oneOrNone('SELECT * FROM users WHERE login = $1', username);
+      console.log('по api пришло: ', req.body.data)
+      console.log('из базы пришло: ', login_user)
+      
+      const match = await bcrypt.compare(password, login_user.password);
+      console.log(match)
+      
+      
+    if (login_user !== null && login_user.login === username && match)
       {
         console.log('Пользователь найден');
         res.status(200).json({ success: true, user: login_user });
@@ -53,39 +54,12 @@ app.post('/registration', async (req, res) => {
   console.log(req.body.data);
   
   try {
-      const hashedPassword = await bcrypt.hash(password, 10);
-    
-      await db.none('INSERT INTO users(login, password) VALUES($1, $2)', [username, hashedPassword]);
+      const hashPassword = await bcrypt.hash(password, 10);
+      await db.none('INSERT INTO users(login, password) VALUES($1, $2)', [username, hashPassword]);
       res.json({ message: 'Пользователь успешно зарегистрирован' });
   } 
   catch (error) {
-    console.error('Ошибка регистрации пользователя:', error);
-    res.status(500).json({ error: 'Ошибка сервера' });
+      console.error('Ошибка регистрации пользователя:', error);
+      res.status(500).json({ error: 'Ошибка сервера' });
   }
 });
-
-    // Получаем данные пользователя из базы данных
-   
-    // if (!user) {
-    //    return res.status(401).json({ error: 'Неверное имя пользователя или пароль' });
-    // }
-
-
-    // Сравниваем хэши паролей
-  //   const match = await bcrypt.compare(password, user.password);
-  //   if (!match) {
-  //     return res.status(401).json({ error: 'Неверное имя пользователя или пароль' });
-  //   }
-  //   // Генерируем JWT токен
-  //   const token = jwt.sign({ username: user.username }, 'secret');
-  //   res.json({ token });
-  // } catch (error) {
-  //   console.error('Ошибка аутентификации пользователя:', error);
-  //   res.status(500).json({ error: 'Ошибка сервера' });
-  //}
-
-
-
-
-
-

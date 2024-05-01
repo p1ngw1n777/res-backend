a1209a16d1a0f106b8ace3c4d4f9645d46c80153
pingwin <pingwin@MacBook-Air-Medvedev.local>
2024-05-01 21:42:26 +0700

    feat: added left-menu category page functionality

--- server.js
+++ server.js
@@ -17,12 +17,35 @@ app.listen(port, () => {
   console.log(`Сервер запущен на порту ${port}`);
 });
 
+//Список категории
+app.get('/catalog', async (req, res) => {
+  try {
+    const categoryFromDB = await db.any('SELECT category_name, category_parent FROM category');
+    console.log('из бд', categoryFromDB);
+
+    if(categoryFromDB){
+        const defaultCategory = [
+            {
+                category_name: 'defaultName',
+                category_parent: 'defautlCategory',
+            }
+        ]
+    }
+
+    res.status(200).json({ categoryFromDB });
+  }
+  catch (error){
+    res.status(500).json({ success: false, message: 'Ошибка при выполнении запроса' });
+  }
+});
+
 //Отзывы
 app.get('/', async (req, res) => {
   try {
     const reviewsFromDB = await db.any('SELECT photo, name, surname, text, wphoto FROM reviews');
-    const categoryFromDB = await db.any('SELECT category_name, category_url_photo FROM category');
+    const categoryFromDB = await db.any('SELECT category_name, category_url_photo, category_parent FROM category');
 
+    console.log(categoryFromDB)
     if (reviewsFromDB) {
       // Если данные из базы данных пусты, отправляем данные по умолчанию
       const defaultData = [
